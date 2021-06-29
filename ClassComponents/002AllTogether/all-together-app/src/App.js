03648a3d8e02dc579cf220c124f9ecaf7481fc7d
Juan Jose Ramirez <juanjose.rami25@gmail.com>
2021-06-28 21:17:28 -0400

    adding activity all together

--- ClassComponents/002AllTogether/all-together-app/src/App.js
+++ ClassComponents/002AllTogether/all-together-app/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import PersonCard from './classComponents/personCard'
 
 function App() {
+  const person = [
+    { firstName: 'Jane', lastName: 'Doe', age: 20, hairColor: 'Black' },
+    { firstName: 'Smith', lastName: 'Joe', age: 88, hairColor: 'Brown' }
+  ]
+
+
   return (
     <div className="App">
-
+      {person.map((person, index) => (
+        <PersonCard key={index} {...person} />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ ClassComponents/002AllTogether/all-together-app/src/classComponents/personCard.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+class personCard extends React.Component {
+    // PROPS ES UN ATRIBUTO QUE VIENE DE LA CLASE COMPONENTS
+    constructor(props) {
+        super(props)
+        this.state = { currentAge: null }
+    }
+
+    // metodo de la clase (pripia)
+    handleOnClick() {
+        // destructuramos
+        const {
+            props: { age },
+            state: { currentAge } } = this
+
+        // Operacion ternaria
+        // const initialAge = !currentAge ? age : currentAge
+        const initialAge = currentAge || age
+        this.setState(() => ({ currentAge: initialAge + 1 }))
+    }
+
+
+
+
+
+
+    //METODO HEREDADO
+    render() {
+        // destructuramos el props
+        const { firstName, lastName, age, hairColor } = this.props
+        const { currentAge } = this.state
+        return (
+            <div>
+                <h1>
+                    {lastName} ,{firstName}
+                </h1>
+                <p>
+                    Age: {currentAge || age}
+                </p>
+                <p>
+                    <button onClick={() => this.handleOnClick()} >
+                        Birthday button for {firstName} {lastName}
+                    </button>
+                </p>
+            </div>
+        )
+    }
+
+}
+
+export default personCard
